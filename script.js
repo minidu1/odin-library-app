8e2433c3ecade53b15b70678fac4e3836ccabb41
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2026-01-23 22:09:23 +0530

    add js to make empty card

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ addBookToLibrary("1984", "George Orwell", 328)
 addBookToLibrary("Brave New World", "Aldous Huxley", 288)
 
 myLibrary.forEach(book => console.log(book))
+
+const library = document.querySelector(".library")
+
+const card = document.createElement("div") 
+card.classList.add("book-card")
+library.appendChild(card)
+
+const bookName =  document.createElement("div")
+bookName.classList.add("name")
+card.appendChild(bookName)
+
+const content = document.createElement("div")
+content.classList.add("content")
+card.appendChild(content)
