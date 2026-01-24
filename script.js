d417c9eef980bac73bcb433d4c69c507bf79c975
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2026-01-24 16:10:18 +0530

    add grid to make responsive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = []
 
-function Book(title, author, pages){
+function Book(title, author, pages) {
     if (!new.target) {
         throw Error("you need to use 'new' operator to call the constructor")
     }
@@ -19,25 +19,27 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310)
 addBookToLibrary("1984", "George Orwell", 328)
 addBookToLibrary("Brave New World", "Aldous Huxley", 288)
 
-myLibrary.forEach(book => console.log(book))
-
 const library = document.querySelector(".library")
 
-const card = document.createElement("div") 
-card.classList.add("book-card")
-library.appendChild(card)
+myLibrary.forEach((book) => {
 
-const bookName =  document.createElement("div")
-bookName.classList.add("name")
-card.appendChild(bookName)
-bookName.textContent = "book name"
+    const card = document.createElement("div")
+    card.classList.add("book-card")
+    library.appendChild(card)
 
-const author = document.createElement("div")
-author.classList.add("content")
-card.appendChild(author)
-author.textContent = "author"
+    const bookName = document.createElement("div")
+    bookName.classList.add("name")
+    card.appendChild(bookName)
+    bookName.textContent = book.title
 
-const pages = document.createElement("div")
-pages.classList.add("content")
-card.appendChild(pages)
-pages.textContent = "xxx pages"
+    const author = document.createElement("div")
+    author.classList.add("content")
+    card.appendChild(author)
+    author.textContent = `by ${book.author}`
+
+    const pages = document.createElement("div")
+    pages.classList.add("content")
+    card.appendChild(pages)
+    pages.textContent = `${book.pages} pages`
+
+})
